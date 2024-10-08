b8ae56e8390b11967b24a4feadb44f7374f5aa50
Emeka Eric <emekaeric12@gmail.com>
2024-10-08 08:20:56 +0100

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+   <div className="h-screen w-full relative">
+    <img src="" className="h-full w-full object-cover absolute top-0 left-0" alt="" />
+   </div>
+  );
+}
